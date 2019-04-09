bbc98ef270d9304782612b6d48ff536b193def28
Jonathan Chang <jchang641@gmail.com>
2019-04-09 17:01:23 +1000

    Update

--- cmd/brew-core-merge.rb
+++ cmd/brew-core-merge.rb
@@ -11,10 +11,40 @@ module Homebrew
     end
   end
 
+  def passed_ci?(pr)
+    statuses = GitHub.open_api(pr["statuses_url"])
+
+    latest_pr_status = statuses.select { |status| status["context"] == "continuous-integration/jenkins/ghprb" }
+      .max_by { |status| Time.parse(status["updated_at"]) }
+
+    latest_pr_status&.fetch("state") == "success"
+  end
+
+  def ready_to_merge?(pr)
+    pr["labels"].each do |label|
+      return true if label["name"] == "ready to merge" and label["id"] == 778631352
+    end
+    return false
+  end
+
   def core_merge
     core_merge_args.parse
 
-    ohai "Dry run" if args.dry_run?
+    core_name = CoreTap.instance.full_name
+    odie "This command may only be run by maintainers" unless GitHub.write_access? core_name
+
+    open_pull_requests = GitHub.pull_requests(core_name, state: :open, base: "master")
+
+    ready_prs = open_pull_requests.collect do |pr|
+      next unless ready_to_merge? pr
+      next unless passed_ci? pr
+      pr["number"]
+    end
+    ready_prs.compact!
+
+    ohai "Now run:"
+    puts "brew pull --bottle #{ready_prs.join(' ')}"
+    puts "git log -u origin/master"
   end
 end
 
